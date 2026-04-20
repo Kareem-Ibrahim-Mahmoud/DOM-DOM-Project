707920c4b9f552c753b024fe1dfd09d97b92e911
kareem ibrahim <kareemimrahim@gmail.com>
2026-04-20 14:22:08 +0200

    Upload Layer 4 Finish

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace DOM_DOM_Team_Qudra_Saas
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
